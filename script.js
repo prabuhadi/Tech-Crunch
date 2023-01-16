9c83340f863f50c3ffcde0e48abff8d438feb88c
prabuhadi <prabuhadiyo@gmail.com>
2023-01-16 11:51:25 +0700

    Fix image, create slide banner and animation

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(`HALLOO TES`);
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
